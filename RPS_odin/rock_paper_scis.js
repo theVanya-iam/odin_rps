fe5a2e45316af5b6dd5ab3efd2f6156a8d7f76d5
Vanya_Rom <ivan@romanenko.in>
2022-10-13 13:24:47 -0400

    title counts rounds during game

--- RPS_odin/rock_paper_scis.js
+++ RPS_odin/rock_paper_scis.js
@@ -2,9 +2,9 @@ const buttons = document.querySelectorAll('.button');
 const rock = document.querySelector('.buttonRock');
 let scoreBoard = document.querySelector('.score');
 let results = document.querySelector('.results');
-let roundBoard = document.querySelector('.round');
 let playerBoard = document.querySelector('.selection');
 let computerBoard = document.querySelector('.botSelection');
+let title = document.querySelector('.title');
 
 let user_score = 0;
 let computer_choice;
@@ -13,12 +13,6 @@ const rounds = 5;
 const noWinner = 'There is no winner! It\'s a tie';
 const userWinner = 'Computer is DED, you get 1 point!';
 const userLoser = 'Computer WINs, finish him VISTA!';
-
-// 1) Page states what to do
-// 2) Page plays the game and tells you the result
-// 3) Score is adjusted
-// 4) Next round starts
-// 5) Game stops declaring final results after Round 5
 
 function getRandomInt (min, max) {
     min = Math.ceil(min);
@@ -61,7 +55,7 @@ function choiceMade() {
     user_choice = this.textContent.toLowerCase();
     chooseWinner();
     console.log({user_choice, computer_choice, user_score});
-    roundBoard.textContent = `Round: ${roundCount}`;
+    title.textContent = `Round: ${roundCount}`;
     playerBoard.textContent = `Player: ${user_choice}`;
     ++roundCount;
 };
@@ -74,7 +68,31 @@ function playGame() {
         buttons.forEach(button => button.removeEventListener('click', choiceMade));
         buttons.forEach(button => button.removeEventListener('click', playGame));
         scoreBoard.textContent = user_score > 0 ? 'You win!' : 'Computer wins!';
-    };
+        title.classList.remove = 'round';
+        results.textContent = '';
+    } else if (user_score === -5){
+        results.textContent = 'Wow, you have reached the lowest point humanity thought was unreachable.';
+    } else if (user_score === -4){
+        results.textContent = 'How much lower can you go?';
+    } else if (user_score === -3){
+        results.textContent = 'Computers likely will prevail!';
+    } else if (user_score === -2){
+        results.textContent = 'Ouch, it hurts to lose to computer!';
+    } else if (user_score === -1){
+        results.textContent = 'Computer won!';
+    } else if (user_score === 0){
+        results.textContent = '';
+    } else if (user_score === 1){
+        results.textContent = 'You got this!';
+    } else if (user_score === 2){
+        results.textContent = 'You scored two!';
+    } else if (user_score === 3){
+        results.textContent = 'Hurray!';
+    } else if (user_score === 4){
+        results.textContent = 'Damn, you may be the best player ever.';
+    } else if (user_score === 5){
+        results.textContent = 'YOU ARE.';
+    }
 
     return;
 }; 
